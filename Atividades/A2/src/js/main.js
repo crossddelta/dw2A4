37543bbb29d4d7c093881df28d9b32333c75b7f2
crossddelta <danilo.lessa@aluno.ifsp.edu.br>
2022-09-08 01:52:41 -0300

    Adicionando scripts

--- Atividades/A2/src/js/main.js
+++ Atividades/A2/src/js/main.js
@@ -15,5 +15,37 @@ const Modal = {
         .classList
         .remove('active')
     }
+}
 
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '07/09/2022'
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '07/09/2022'
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '07/09/2022'
+    }
+]
+
+const Transaction = {
+    incomes(){
+        //somar as entradas
+    },
+    expenses(){
+        //somar as saídas
+    },
+    total(){
+        //entradas - saídas
+    }
 }
