e66a9ad333b4fcb4cabfb1b051535c2f0109c53f
crossddelta <danilo.lessa@aluno.ifsp.edu.br>
2022-10-09 02:01:47 -0300

    Enviando ambiente configurado React

--- Atividades/A5/babel.config.js
+++ Atividades/A5/babel.config.js
@@ -1,3 +1,9 @@
-module.export ={
-    presets:['@babel/preset-env', '@babel/preset-react', '@babel/plugin-transform-typeof-symbol']
+module.exports = {
+    presets: [
+        '@babel/preset-env', 
+        ['@babel/preset-react', {
+            runtime: 'automatic'
+            }
+        ]
+    ]
 }
