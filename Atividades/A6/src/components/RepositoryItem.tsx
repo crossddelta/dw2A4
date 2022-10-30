6298c5a6759d251f2386e69328bfbd1a6803c5e9
crossddelta <danilo.lessa@aluno.ifsp.edu.br>
2022-10-30 02:09:47 -0300

    Introdução ao React 11-21

---
+++ Atividades/A6/src/components/RepositoryItem.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface RepositoryItemProps{
+    repository:{
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+
+export function RepositoryItem(props: RepositoryItemProps){
+    return(
+        <li>
+            <strong>{props.repository?.name ?? 'Repositório Padrão'}</strong>
+            <p>{props.repository?.description}</p>
+            <a href={props.repository?.html_url}>Acesso ao repositório</a>
+        </li>
+    )
+}
